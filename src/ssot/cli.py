1ed6aa6f351e3bd8e2239c2906e694989ae848a7
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-08 22:56:46 -0400

    Prototype

--- src/ssot/cli.py
+++ src/ssot/cli.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 import tomllib
+import re
 
 ssot = "ssot.toml"
-target = "target.txt"
+target = "target.yaml"
+bracket_regex = r"\[\[ (.*?) \]\]"
 
 def main():
+    ssot_values = None
+    
+    # Load dictionary from SSoT
     with open(ssot, "rb") as f:
-        data = tomllib.load(f)
-        print(data)
+        ssot_values = tomllib.load(f)
 
+    # open target
+    with open(target, "r+") as f:
+        data = f.read()
+    
+
+        pattern = re.compile(bracket_regex)
+        print(pattern.findall(data))
+        
+        
 if __name__ == "__main__":
     main()
