bd1a948f12f214859ecab28618c1a96affe967c6
Juan Urdaneta <jurdanetac10@gmail.com>
2026-08-13 22:22:12 -0400

    Get replacing placeholders on templates working

--- src/ssot/cli.py
+++ src/ssot/cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import copy
 import re
 import sys
 import tomllib
@@ -9,14 +10,54 @@ from pathlib import Path
 BRACKET_PLACEHOLDER_RE = re.compile(r"\[\[ (.*?) \]\]")
 TPL_FILE_RE = re.compile(r"(.*\.tpl)", re.IGNORECASE)
 
-def main(args):
+
+class DotDict(dict):
+    def __getattr__(self, name):
+        if name.startswith("__") and name.endswith("__"):
+            raise AttributeError(name)
+
+        return self[name]
+
+    def get_dot(self, dot):
+        """get_dot("a.b.c")"""
+
+        value = copy.deepcopy(self)
+        to_pop = dot.split(".")
+
+        while to_pop:
+            next_key = to_pop.pop(0)
+            value = value[next_key]
+
+        return value
+
+
+def substitute_brackets(dictionary, text) -> str:
+    matches = BRACKET_PLACEHOLDER_RE.findall(text)
+
+    if not matches:
+        print("No brackets found.")
+        return
+
+    dot_dict = DotDict(dictionary)
+
+    for m in matches:
+        value = str(dot_dict.get_dot(m))
+        text = text.replace(f"[[ {m} ]]", value)
+
+    return text
+
+
+def main(args) -> None:
     ssot_file = args[0]
     ssot_dictionary = None
-    
+
     # Load dictionary from SSoT
     with open(ssot_file, "rb") as f:
         ssot_dictionary = tomllib.load(f)
+        print(f"INFO: using dictionary {ssot_dictionary}")
+        print()
 
+    # Path of template(s) or enclosing folder
     target = Path(args[1]).resolve()
     if target.is_dir():
         templates = [f for f in target.iterdir() if TPL_FILE_RE.match(f.suffix)]
@@ -25,12 +66,14 @@ def main(args):
             for tpl in templates:
                 print(tpl)
     elif target.is_file():
-        print("file")
+        with open(target, "r") as template:
+            text = template.read()
+            print(substitute_brackets(ssot_dictionary, text))
     else:
         print(f"Target '{target}' neither directory nor file.")
         sys.exit(1)
 
-    
+
 if __name__ == "__main__":
     args = sys.argv[1:]
 
